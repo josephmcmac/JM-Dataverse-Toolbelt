e33b0d24813c0143b4d1cde340d9f60ae2ed1691
josephmcmac <josephmcmac@gmail.com>
2015-05-26 15:53:00 +1000

    Added Sort Form Grid Section When Click Column Header

--- JosephM.Record.Application/Grid/DynamicGridViewModeltems.cs
+++ JosephM.Record.Application/Grid/DynamicGridViewModeltems.cs
@@ -1,6 +1,11 @@
 ﻿#region
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Annotations;
+using System.Windows.Documents;
+using JosephM.Core.Extentions;
 
 #endregion
 
@@ -38,5 +43,78 @@ namespace JosephM.Record.Application.Grid
         public Action<GridRowViewModel> EditRow { get; set; }
         public Action OnDoubleClick { get; set; }
         public Action OnKeyDown { get; set; }
+
+        private string _lastSortField;
+        private bool _lastSortAscending = false;
+        /// <summary>
+        /// sorts the grid
+        /// put in here to avoid repeating for each concrete IDynamicGridViewModel type
+        /// </summary>
+        /// <param name="theGridViewModel"></param>
+        /// <param name="sortField"></param>
+        public void SortIt(IDynamicGridViewModel theGridViewModel, string sortField)
+        {
+            //todo never added any validation script for this
+
+            //just copy all into a new list (sorting as go), clear the collection then add each sorted item
+            if (theGridViewModel.GridRecords.Any())
+            {
+                var newList = new List<GridRowViewModel>();
+                foreach (var item in theGridViewModel.GridRecords)
+                {
+                    var value1 = item.GetFieldViewModel(sortField).ValueObject;
+                    if (value1 == null)
+                    {
+                        newList.Insert(0, item);
+                        continue;
+                    }
+                    foreach (var item2 in newList)
+                    {
+                        var value2 = item2.GetFieldViewModel(sortField).ValueObject;
+                        if (value2 == null)
+                        {
+                            continue;
+                        }
+                        else if (!(value1 is Enum) && value1 is IComparable)
+                        {
+                            if (((IComparable)value1).CompareTo(value2) < 0)
+                            {
+                                newList.Insert(newList.IndexOf(item2), item);
+                                break;
+                            }
+                            else
+                                continue;
+                        }
+                        var sortString1 = value1.ToString();
+                        var sortString2 = value2.ToString();
+                        if (value1 is Enum)
+                            sortString1 = ((Enum) value1).GetDisplayString();
+                        if (value2 is Enum)
+                            sortString2 = ((Enum)value2).GetDisplayString();
+                        if (String.Compare(sortString1, sortString2, StringComparison.Ordinal) < 0)
+                        {
+                            newList.Insert(newList.IndexOf(item2), item);
+                            break;
+                        }
+                    }
+                    if(!newList.Contains(item))
+                        newList.Add(item);
+                }
+                //just a check for already sorted ascending the sort descending
+                if (_lastSortField != sortField)
+                    _lastSortAscending = true;
+                else
+                {
+                    if (_lastSortAscending)
+                        newList.Reverse();
+                    _lastSortAscending = !_lastSortAscending;
+                }
+                _lastSortField = sortField;
+                
+                theGridViewModel.GridRecords.Clear();
+                foreach (var item in newList)
+                    theGridViewModel.GridRecords.Add(item);
+            }
+        }
     }
 }
--- JosephM.Wpf/RecordEntry/GridSectionView.xaml.cs
+++ JosephM.Wpf/RecordEntry/GridSectionView.xaml.cs
@@ -1,6 +1,9 @@
 ﻿#region
 
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using JosephM.Record.Application.Grid;
 using JosephM.Wpf.Grid;
 
 #endregion
@@ -21,5 +24,24 @@ namespace JosephM.Wpf.RecordEntry
         {
             get { return DataGrid; }
         }
+
+        protected IDynamicGridViewModel DynamicGridViewModel
+        {
+            get { return DataContext as IDynamicGridViewModel; }
+        }
+
+        private void columnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            var columnHeader = sender as DataGridColumnHeader;
+            if (columnHeader != null)
+            {
+                var sortMember = columnHeader.Column.SortMemberPath;
+                if (sortMember != null)
+                {
+                    DynamicGridViewModel.DynamicGridViewModelItems.SortIt(DynamicGridViewModel, sortMember.Trim(new[] {'[', ']'}));
+                }
+            }
+        }
+
     }
 }
