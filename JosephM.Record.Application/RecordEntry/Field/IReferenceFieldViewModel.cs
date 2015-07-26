fd3b83e5aa9f77d9353ad34dfa9d0336e0e30b5b
josephmcmac <josephmcac@gmail.com>
2015-07-27 08:42:38 +1000

    Added Missing Class Source

---
+++ JosephM.Record.Application/RecordEntry/Field/IReferenceFieldViewModel.cs
@@ -0,0 +1,32 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using JosephM.Core.Extentions;
+using JosephM.Core.FieldType;
+using JosephM.Record.Application.Grid;
+using JosephM.Record.Application.RecordEntry.Form;
+using JosephM.Record.Application.Shared;
+using JosephM.Record.IService;
+using JosephM.Record.Query;
+
+#endregion
+
+namespace JosephM.Record.Application.RecordEntry.Field
+{
+    public interface IReferenceFieldViewModel
+    {
+        string RecordTypeToLookup { get; set; }
+
+        IRecordService LookupService { get; }
+
+        RecordEntryViewModelBase RecordEntryViewModel { get; }
+
+        void Search();
+
+        void SelectLookupGrid();
+
+        bool Searching { get; }
+    }
+}
