a8008d9b5141cc473389666b7bf24f36ee514fcb
Joseph McGregor-MacDonald <josephmcmac@gmail.com>
2017-10-17 08:08:29 +1100

    Adjustments to form heights and scrolling

---
+++ JosephM.Wpf/Converters/LengthConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace JosephM.Wpf.Converters
+{
+    public class LengthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = (double) 100;
+            if (parameter == null)
+                parameter = "1";
+            if (value is double && parameter != null)
+                result = (double) value*double.Parse(parameter.ToString());
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+}
