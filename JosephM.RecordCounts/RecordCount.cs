2378dff0666cb2f4072dcf47bf97fee79d12b0ff
josephmcmac <josephmcac@gmail.com>
2017-10-28 15:13:32 +1100

    Adjusted record count dialog to display results in UI with download option

--- JosephM.RecordCounts/RecordCount.cs
+++ JosephM.RecordCounts/RecordCount.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JosephM.Core.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,10 @@ namespace JosephM.RecordCounts
             Count = count;
         }
 
+        [DisplayOrder(20)]
         public string RecordType { get; set; }
+        [DisplayOrder(30)]
+        [GridWidth(125)]
         public long Count { get; set; }
     }
 }
