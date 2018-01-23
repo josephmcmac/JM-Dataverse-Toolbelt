adf0d480ac65cbd7f30fcba978e5fcaf9af0bacc
Joseph McGregor-MacDonald <josephmcmac@gmail.com>
2018-01-23 11:59:32 +1100

    Added custom button to generate csv import templates in import csv dialog

---
+++ JosephM.Deployment/ImportCsvs/GenerateTemplatesRequest.cs
@@ -0,0 +1,28 @@
+﻿using JosephM.Core.Attributes;
+using JosephM.Core.FieldType;
+using System.Collections.Generic;
+
+namespace JosephM.Deployment.ImportCsvs
+{
+    [Group(Sections.Main, true)]
+    public class GenerateTemplatesRequest
+    {
+        [Group(Sections.Main)]
+        [DisplayOrder(10)]
+        [RequiredProperty]
+        public Folder FolderToSaveInto { get; set; }
+
+        [Group(Sections.Main)]
+        [DisplayOrder(20)]
+        [RequiredProperty]
+        public bool UseSchemaNames { get; set; }
+
+        [RequiredProperty]
+        public IEnumerable<GenerateTemplateConfiguration>  CsvsToGenerate { get; set; }
+
+        private static class Sections
+        {
+            public const string Main = "Main";
+        }
+    }
+}
