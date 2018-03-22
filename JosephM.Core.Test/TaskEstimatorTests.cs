5eed99f341e85e6174a475ccaa96b93091e2c2db
Joseph McGregor-MacDonald <josephmcmac@gmail.com>
2018-03-23 09:08:32 +1100

    Added Remaining Count And Time Estimation To Bulk Update & Delete Dialogs

---
+++ JosephM.Core.Test/TaskEstimatorTests.cs
@@ -0,0 +1,24 @@
+﻿using JosephM.Core.Utility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Threading;
+
+namespace JosephM.Core.Test
+{
+    [TestClass]
+    public class TaskEstimatorTests : CoreTest
+    {
+        [TestMethod]
+        [DeploymentItem("app.config")]
+        public void TaskEstimatorTestsTest()
+        {
+            var iterations = 2;
+            var taskEstimator = new TaskEstimator(iterations);
+            for(var i = 0; i < iterations; i++)
+            {
+                Thread.Sleep(1000);
+                Debug.WriteLine($"Remaining {taskEstimator.GetProgressString(i + 1, "Executing Test")}");
+            }
+        }
+    }
+}
