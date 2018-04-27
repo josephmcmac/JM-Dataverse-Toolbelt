2a0d72bd49380ad9b973b83405b4fd3efc4d9e5f
Joseph McGregor-MacDonald <josephmcmac@gmail.com>
2018-04-27 12:57:05 +1000

    Added drag of files and folders into relevant field types + validate filemask extention

--- JosephM.Core/Attributes/IValidatableObject.cs
+++ JosephM.Core/Attributes/IValidatableObject.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using JosephM.Core.Service;
+﻿using JosephM.Core.Service;
 
 namespace JosephM.Core.Attributes
 {
