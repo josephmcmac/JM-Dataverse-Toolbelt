67955aba0b3224d605401971bf384a1cabcba232
Joseph McGregor-MacDonald <josephmcmac@gmail.com>
2018-09-04 11:38:13 +1000

    Relabel Settings Dialog Button

--- JosephM.Prism.Infrastructure/Module/Settings/SettingsModule.cs
+++ JosephM.Prism.Infrastructure/Module/Settings/SettingsModule.cs
@@ -1,6 +1,7 @@
 ﻿using JosephM.Application.Application;
 using JosephM.Application.Desktop.Module.Dialog;
 using JosephM.Core.AppConfig;
+using JosephM.Core.Extentions;
 
 namespace JosephM.Application.Desktop.Module.Settings
 {
@@ -22,6 +23,15 @@ namespace JosephM.Application.Desktop.Module.Settings
         public override void InitialiseModule()
         {
             AddSetting(MainOperationName, DialogCommand, OperationDescription);
+        }
+
+        public override string MainOperationName
+        {
+            get
+            {
+                var typeName = (typeof(TSettingsDialog)).GetDisplayName();
+                return typeName.EndsWith(" Dialog") ? typeName.Substring(0, typeName.Length - 7) : typeName;
+            }
         }
     }
 }
