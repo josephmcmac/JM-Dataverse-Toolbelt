91cee0d10e6375e1411121312e0ddbe7c2f57cee
Joseph McGregor-MacDonald <josephmcmac@gmail.com>
2018-09-04 13:08:30 +1000

    Labelled Update VM with record name + truncate tab label at 30 chars

---
+++ JosephM.Wpf/Converters/StringTruncateConverter.cs
@@ -0,0 +1,32 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+
+#endregion
+
+namespace JosephM.Wpf.Converters
+{
+    public class StringTruncateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(parameter is int && value is string)
+            {
+                var maxChars = (int)parameter;
+                var theString = (string)value;
+                if (theString.Length > maxChars)
+                    theString = theString.Substring(0, maxChars - 2) + "...";
+                return theString;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
