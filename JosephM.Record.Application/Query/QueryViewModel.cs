51ba4acaa92e24652fee48dbc6bbef11e2fe1a8d
Joseph McGregor-MacDonald <josephmcmac@gmail.com>
2018-11-22 14:35:37 +1100

    In query / crud dialog populate names in reference fields which dynamics web service hasnt populated them

--- JosephM.Record.Application/Query/QueryViewModel.cs
+++ JosephM.Record.Application/Query/QueryViewModel.cs
@@ -126,8 +126,9 @@ namespace JosephM.Application.ViewModel.Query
                                 DynamicGridViewModel.ReloadGrid();
                             };
                             var record = selectedRow.Record;
-
-                            var newForm = new CreateOrUpdateViewModel(RecordService.Get(record.Type, record.Id), formController, onSave, ClearChildForm);
+                            var createOrUpdateRecord = RecordService.Get(record.Type, record.Id);
+                            new[] { createOrUpdateRecord }.PopulateEmptyLookups(RecordService, null);
+                            var newForm = new CreateOrUpdateViewModel(createOrUpdateRecord, formController, onSave, ClearChildForm);
                             LoadChildForm(newForm);
                         }
                     };
@@ -140,7 +141,9 @@ namespace JosephM.Application.ViewModel.Query
                         {
                             var record = selectedRow.Record;
                             CreateOrUpdateViewModel vmRef = null;
-                            vmRef = new CreateOrUpdateViewModel(RecordService.Get(record.Type, record.Id), formController, () => {
+                            var createOrUpdateRecord = RecordService.Get(record.Type, record.Id);
+                            new[] { createOrUpdateRecord }.PopulateEmptyLookups(RecordService, null);
+                            vmRef = new CreateOrUpdateViewModel(createOrUpdateRecord, formController, () => {
                                 vmRef.ValidationPrompt = "Changes Have Been Saved";
                                 DynamicGridViewModel.ReloadGrid();
                             }, () => ApplicationController.Remove(vmRef), cancelButtonLabel: "Close");
@@ -417,6 +420,7 @@ namespace JosephM.Application.ViewModel.Query
             if (!DynamicGridViewModel.HasPaging || ignorePages)
             {
                 var records = DynamicGridViewModel.RecordService.RetreiveAll(query);
+                records.PopulateEmptyLookups(RecordService, null);
                 return new GetGridRecordsResponse(records);
             }
             else
