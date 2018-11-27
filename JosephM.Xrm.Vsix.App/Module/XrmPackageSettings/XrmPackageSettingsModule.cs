7814277a01ac96f9f7eddd46028904d9e75f379a
Joseph McGregor-MacDonald <josephmcmac@gmail.com>
2018-11-27 13:35:53 +1100

    Added button to copy settings change code to clipboard

--- JosephM.Xrm.Vsix.App/Module/XrmPackageSettings/XrmPackageSettingsModule.cs
+++ JosephM.Xrm.Vsix.App/Module/XrmPackageSettings/XrmPackageSettingsModule.cs
@@ -1,15 +1,40 @@
 ﻿using JosephM.Application.Modules;
 using JosephM.Application.Desktop.Module.Settings;
 using JosephM.XrmModule.XrmConnection;
+using System;
+using JosephM.Application.ViewModel.Extentions;
+using JosephM.Application.ViewModel.RecordEntry.Form;
+using System.Windows.Forms;
 
 namespace JosephM.Xrm.Vsix.Module.PackageSettings
 {
     [DependantModule(typeof(XrmConnectionModule))]
     public class XrmPackageSettingsModule : SettingsModule<XrmPackageSettingsDialog, XrmPackageSettings, XrmPackageSettings>
     {
+        public override void RegisterTypes()
+        {
+            base.RegisterTypes();
+            AddCopyNewCodeButton();
+        }
+
         public override void DialogCommand()
         {
             ApplicationController.NavigateTo(typeof(XrmPackageSettingsDialog), null);
+        }
+
+        private void AddCopyNewCodeButton()
+        {
+            this.AddCustomFormFunction(new CustomFormFunction("CopyCode", "Copy C# To Clipboard", (r) =>
+            {
+                try
+                {
+                    Clipboard.SetText(r.GetStringFieldFieldViewModel(nameof(SettingsFolderMoving.WithTheseLines)).Value);
+                }
+                catch(Exception ex)
+                {
+                    r.ApplicationController.ThrowException(ex);
+                }
+            }), typeof(SettingsFolderMoving));
         }
     }
 }
