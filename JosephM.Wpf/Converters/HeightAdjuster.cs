5f1be2b9ddf98108f5e493118984cb89fc29e55f
Joseph McGregor-MacDonald <josephmcmac@gmail.com>
2019-03-14 16:06:51 +1100

    Issue #118 improve scrolling in query view

---
+++ JosephM.Wpf/Converters/HeightAdjuster.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace JosephM.Wpf.Converters
+{
+    public class HeightAdjuster : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = (double) 100;
+            if (value is double && parameter != null)
+                result = (double) value+double.Parse(parameter.ToString());
+            if(result < 20)
+                result = (double)20;
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+}
