b9c2e1abdbd2826890ded82e2c7f289b5ca9f0b6
Joseph McGregor-MacDonald <josephmcmac@gmail.com>
2019-03-21 14:48:21 +1100

    Update descriptions of use logging + display update setting sin compact view

--- JosephM.Prism.Infrastructure/Module/ApplicationInsights/ApplicationInsightsSettings.cs
+++ JosephM.Prism.Infrastructure/Module/ApplicationInsights/ApplicationInsightsSettings.cs
@@ -2,7 +2,7 @@
 
 namespace JosephM.Application.Desktop.Module.ApplicationInsights
 {
-    [Instruction("This application logs anonymous usage statistics and errors to application insights. Some location information is automatically captured but no user or machine names are included in the data submitted\n\nThe logged data is used by the author to improve functionality, count feature use, and identify unknown errors being thrown by the application\n\nIf you do not want the application to log use set the flag to false")]
+    [Instruction("This setting is for information on feature use and errors being submitted to application insights. Some location information is automatically captured but unless you opt in your user and machine name is not included in the data submitted\n\nThe logged data is used by the author to improve functionality, count feature use, and identify unknown errors being thrown by the application\n\nIf you do not want any of your use logged set the flag to false. To allow your username to be included in the information submitted please set this option. This will assist with reporting on numbers of users")]
     [Group(Sections.Main, wrapHorizontal:true)]
     public class ApplicationInsightsSettings
     {
--- JosephM.Prism.Infrastructure/Module/ReleaseCheckModule/UpdateSettings.cs
+++ JosephM.Prism.Infrastructure/Module/ReleaseCheckModule/UpdateSettings.cs
@@ -1,5 +1,8 @@
-﻿namespace JosephM.Application.Desktop.Module.ReleaseCheckModule
+﻿using JosephM.Core.Attributes;
+
+namespace JosephM.Application.Desktop.Module.ReleaseCheckModule
 {
+    [Group(Sections.Main, wrapHorizontal: true)]
     public class UpdateSettings
     {
         public UpdateSettings()
@@ -7,6 +10,13 @@
             CheckForNewReleaseOnStartup = true;
         }
 
+        [Group(Sections.Main)]
+        [DisplayOrder(10)]
         public bool CheckForNewReleaseOnStartup { get; set; }
+
+        public class Sections
+        {
+            public const string Main = "Main";
+        }
     }
 }
