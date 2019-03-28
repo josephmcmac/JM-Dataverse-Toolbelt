55658cd8c9a32026bf7f2199d9d0b0cc71c8baa1
Joseph McGregor-MacDonald <josephmcmac@gmail.com>
2019-03-28 13:57:28 +1100

    Add event log details for customisation export

--- JosephM.CustomisationExporter/Exporter/CustomisationExporterDialog.cs
+++ JosephM.CustomisationExporter/Exporter/CustomisationExporterDialog.cs
@@ -1,8 +1,9 @@
 ﻿using JosephM.Application.Desktop.Module.ServiceRequest;
 using JosephM.Application.ViewModel.Attributes;
 using JosephM.Application.ViewModel.Dialog;
-using JosephM.Core.Extentions;
 using JosephM.Record.Xrm.XrmRecord;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace JosephM.CustomisationExporter.Exporter
 {
@@ -16,6 +17,25 @@ namespace JosephM.CustomisationExporter.Exporter
             XrmRecordService recordService)
             : base(service, dialogController, recordService)
         {
+        }
+
+        protected override IDictionary<string, string> GetPropertiesForCompletedLog()
+        {
+            var dictionary = base.GetPropertiesForCompletedLog();
+            void addProperty(string name, string value)
+            {
+                if (!dictionary.ContainsKey(name))
+                    dictionary.Add(name, value);
+            }
+            addProperty("All Types", Request.IncludeAllRecordTypes.ToString());
+            if(!Request.IncludeAllRecordTypes && Request.RecordTypes != null)
+                addProperty("Selected Type Count", Request.RecordTypes.Count().ToString());
+            addProperty("Entities", Request.Entities.ToString());
+            addProperty("Fields", Request.Fields.ToString());
+            addProperty("Relationships", Request.Relationships.ToString());
+            addProperty("Field Option Sets", Request.FieldOptionSets.ToString());
+            addProperty("Shared Option Sets", Request.SharedOptionSets.ToString());
+            return dictionary;
         }
     }
 }
