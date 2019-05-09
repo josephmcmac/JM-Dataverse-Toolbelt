0d4e4c2b89e416829a1f2022054cf6db65f174cd
Joseph McGregor-MacDonald <josephmcmac@gmail.com>
2019-05-10 09:20:44 +1000

    Issue 150 only display scroll in dialog display area if more than 1 UI item

---
+++ JosephM.Wpf/Converters/ScrollWhereGreaterThanOneConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace JosephM.Wpf.Converters
+{
+    public class ScrollWhereGreaterThanOneConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = ScrollBarVisibility.Hidden;
+            if (value is int)
+                result = ((int)value) > 1 ? ScrollBarVisibility.Auto : ScrollBarVisibility.Hidden;
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+}
