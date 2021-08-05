8341ff16d3d7f25077bbd89ce7bd725d9a927e78
Joseph McGregor-MacDonald <Joseph.Mcgregor-Macd@datacom.com.au>
2021-08-05 21:27:20 +1000

    Remove unqueryable types in record count service

--- JosephM.RecordCounts/RecordCountsService.cs
+++ JosephM.RecordCounts/RecordCountsService.cs
@@ -28,7 +28,7 @@ namespace JosephM.RecordCounts
         {
             controller.LogLiteral("Loading Types");
 
-            var excludeTheseTypes = new[] { Entities.msdyn_componentlayer, Entities.msdyn_solutioncomponentsummary, Entities.msdyn_nonrelationalds, Entities.datalakeworkspace, Entities.datalakeworkspacepermission, Entities.principalobjectaccess, "msdyn_casesuggestion", "msdyn_knowledgearticlesuggestion", "virtualresourcegroupresource", "usermobileofflineprofilemembership", "teammobileofflineprofilemembership", "systemuserauthorizationchangetracker" };
+            var excludeTheseTypes = new[] { Entities.msdyn_componentlayer, Entities.msdyn_solutioncomponentsummary, Entities.msdyn_nonrelationalds, Entities.datalakeworkspace, Entities.datalakeworkspacepermission, Entities.principalobjectaccess, Entities.msdyn_casesuggestion, Entities.msdyn_knowledgearticlesuggestion, Entities.virtualresourcegroupresource, Entities.usermobileofflineprofilemembership, Entities.teammobileofflineprofilemembership, Entities.systemuserauthorizationchangetracker, Entities.searchtelemetry, Entities.appnotification };
 
             var includeTheseTypes = new[] { Entities.incidentresolution };
 
