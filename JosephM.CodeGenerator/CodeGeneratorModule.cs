50931c9efcf98d6d9c55270d441db23751116c56
Joseph McGregor-Macdonald <josephmcmac@gmail.com>
2022-09-14 21:40:18 +1000

    #309 remove Fetch to JavaScript module

--- JosephM.CodeGenerator/CodeGeneratorModule.cs
+++ JosephM.CodeGenerator/CodeGeneratorModule.cs
@@ -1,12 +1,10 @@
 ﻿using JosephM.Application.Modules;
 using JosephM.CodeGenerator.CSharp;
-using JosephM.CodeGenerator.FetchToJavascript;
 using JosephM.CodeGenerator.JavaScriptOptions;
 
 namespace JosephM.CodeGenerator.Xrm
 {
     [DependantModule(typeof(CSharpModule))]
-    [DependantModule(typeof(FetchToJavascriptModule))]
     [DependantModule(typeof(JavaScriptOptionsModule))]
     public class CodeGeneratorModule : ModuleBase
     {
