29b2c8b07d9d723a31327c17ac28cbb7be389176
Joseph McGregor-Macdonald <josephmcmac@gmail.com>
2022-10-28 23:15:12 +1100

    Adjust display of progress and remaining time in bulk tasks

--- JosephM.Core/Utility/TaskEstimator.cs
+++ JosephM.Core/Utility/TaskEstimator.cs
@@ -53,8 +53,8 @@ namespace JosephM.Core.Utility
 
         public string GetProgressString(int countUpdated, string taskName = null)
         {
-            var estimateRemainingString = countUpdated == 0 ? "" : $" - Estimated Time {GetEstimateRemainingDisplayString(countUpdated)}";
-            return $"{(string.IsNullOrWhiteSpace(taskName) ? null : (taskName + " - "))}{ToDo - countUpdated} Remaining{estimateRemainingString}";
+            var estimateRemainingString = countUpdated == 0 ? "" : $"Estimated Time {GetEstimateRemainingDisplayString(countUpdated)}";
+            return $"{(string.IsNullOrWhiteSpace(taskName) ? null : (taskName + "\n\n"))}{ToDo - countUpdated} Remaining\n\n{estimateRemainingString}";
         }
     }
 }
