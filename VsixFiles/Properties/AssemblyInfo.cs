1c712205812e20ab7215db58e153369b99c8c695
Joseph McGregor-Macdonald <josephmcmac@gmail.com>
2022-10-30 00:52:59 +1100

    #324 fix solution template wizard assembly reference, update assembly version to 3.0.0.0

--- VsixFiles/Properties/AssemblyInfo.cs
+++ VsixFiles/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 
 
 //WARNING!! DON:T CHANGE THIS UNLESS ALSO CHANGING THE REFERENCING .vstemplate files
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
